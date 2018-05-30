12199993337b89c187eed286eaafa2d544162b1f
nie-huolin <nie717505221@qq.com>
2018-05-30 19:21:00 +0800

    Initial commit

---
+++ app/src/main/java/com/qbm/www/timepickerdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.qbm.www.timepickerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
